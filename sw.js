f2ae71ff33765e590043a5ece3e846ba447fab32
李海阳 <haiyang6@staff.sina.com.cn>
2018-01-05 14:54:50 +0800

    更新js

---
+++ sw.js
@@ -0,0 +1,34 @@
+
+
+var dataCacheName = 'sinaHome-next-page-v1';
+var cacheName = 'sinaHome-static-v1';
+var filesToCache = [
+  './index.html',
+  './homeinte.min.js',
+  './homeinte.min.css'
+];
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
